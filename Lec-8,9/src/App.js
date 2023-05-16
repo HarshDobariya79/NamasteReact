aacfc06e78604da11d8eb1712e9ca7f06ebf06cd
Harsh Dobariya <harshdobariya79@gmail.com>
2023-05-16 09:01:06 +0530

    Fix minor bugs ADD links for home and about pages

--- Lec-8,9/src/App.js
+++ Lec-8,9/src/App.js
@@ -1,11 +1,12 @@
 import react from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Home from "./components/Home.js";
 import About from "./components/About.js";
 import Footer from "./components/Footer.js";
 import Error from "./components/Error.js";
 import Dynamic from "./components/Dynamic.js";
+import { Link } from "react-router-dom";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
@@ -13,31 +14,35 @@ const AppLayout = () => {
     <>
       <Header />
       <Outlet />
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/about">About</Link>
       <Footer />
     </>
   );
 };
 
 const appRouter = createBrowserRouter([
-    { 
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
         path: "/",
-        element: <AppLayout />,
-        errorElement: <Error />,
-        children: [
-            {
-                path: "/",
-                element: <Home />
-            },
-            {
-                path: "/about",
-                element: <About />
-            },
-            {
-                path: "/:id",
-                element: <Dynamic />
-            }
-        ]
-    }]);
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/:id",
+        element: <Dynamic />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
